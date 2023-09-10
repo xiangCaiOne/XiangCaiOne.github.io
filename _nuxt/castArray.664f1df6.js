e942804cd171c1ba61fca02a619f07afcd11077a
CorianderOne <2683726933@qq.com>
2023-09-10 22:28:42 +0800

    init

--- _nuxt/castArray.664f1df6.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./el-row.d2e20210.js";function e(){if(!arguments.length)return[];var r=arguments[0];return a(r)?r:[r]}export{e as c};
--- _nuxt/index.a58a22ba.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./nuxt-link.2a8d3b16.js";import{_ as o}from"./_plugin-vue_export-helper.c27b6911.js";import{c as n,b as c,w as r,o as _,d as a}from"./entry.8b3a031d.js";const s={};function m(d,f){const e=t;return _(),n("div",null,[c(e,{to:"/home"},{default:r(()=>[a("开始")]),_:1})])}const l=o(s,[["render",m]]);export{l as default};
